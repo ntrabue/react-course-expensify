b28dcdcf22464a6c33480e523718338f722313da
Nick Trabue <ntrabue@flat-branch.com>
2018-01-22 14:05:26 -0600

    Added Andrew's changes and commented out my originals

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -4,20 +4,41 @@ import expenseTotal from '../selectors/expense-total';
 import selectExpenses from '../selectors/expenses';
 import numeral from 'numeral';
 
-export const ExpensesSummary = props => {
-    const expenseWord = props.expenses.length === 1 ? 'expense' : 'expenses';
-    const formattedExpenseTotal = numeral(expenseTotal(props.expenses) / 100).format('$0,0.00')
+//Andrews Version
+export const ExpensesSummary = ({ expenseCount, expensesTotal }) => {
+    const expenseWord = expenseCount === 1 ? 'expense' : 'expenses';
+    const formattedExpensesTotal = numeral(expensesTotal / 100).format('$0,0.00');
+
     return (
         <div>
-            <h2>Viewing {props.expenses.length} {expenseWord} totaling {formattedExpenseTotal}</h2>
+            <h2>Viewing {expenseCount} {expenseWord} totalling {formattedExpensesTotal}</h2>
         </div>
     )
-};
+
+}
+// my version
+// export const ExpensesSummary = props => {
+//     const expenseWord = props.expenses.length === 1 ? 'expense' : 'expenses';
+//     const formattedExpenseTotal = numeral(expenseTotal(props.expenses) / 100).format('$0,0.00')
+//     return (
+//         <div>
+//             <h2>Viewing {props.expenses.length} {expenseWord} totaling {formattedExpenseTotal}</h2>
+//         </div>
+//     )
+// };
 
 const mapStateToProps = state => {
+    //Andrews Version
+    const visibleExpenses = selectExpenses(state.expenses, state.filters);
+
     return {
-        expenses: selectExpenses(state.expenses, state.filters)
-    };
+        expenseCount: visibleExpenses.length,
+        expensesTotal: expenseTotal(visibleExpenses)
+    }
+    // this was my version below:
+    // return {
+    //     expenses: selectExpenses(state.expenses, state.filters)
+    // };
 };
 
 export default connect(mapStateToProps)(ExpensesSummary)
